78bd5200cf830c0a6d25b714f2ebc53e9d84a399
AgaevDanik <agaevdaniik@gmail.com>
2022-02-03 22:31:20 +0100

    implemented database structure and configurations

---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine
+
+engine = create_engine('sqlite:///tmp/chess.db/', echo=True)
+
+metadata_obj = MetaData()
+
+figures = Table('figures', metadata_obj,
+                Column('id', Integer, primary_key=True),
+                Column('name', String, nullable=False),
+                Column('field', String, nullable=False, unique=True),
+                Column('colour', String, nullable=False)
+                )
+
+metadata_obj.create_all(engine)
