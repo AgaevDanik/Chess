6968ea7aa4577059ad02f45c3829e679d9516163
AgaevDanik <agaevdaniik@gmail.com>
2022-02-03 21:57:46 +0100

    project start configuration

---
+++ src/wsgi.py
@@ -0,0 +1,9 @@
+from app import app
+from config import Config
+
+if __name__ == '__main__':
+    app.run(
+        host=Config.APP_HOST,
+        port=Config.APP_PORT,
+        debug=Config.DEBUG_MODE
+    )
